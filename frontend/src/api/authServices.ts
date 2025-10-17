6b2ea848c3698b361d4abf76c022acbafa37e3ca
pratik2061 <pratiksharma2061@gmail.com>
2025-10-17 14:52:40 +0545

    dfd

--- frontend/src/api/authServices.ts
+++ frontend/src/api/authServices.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL:'http://10.40.40.193:3000/api',
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://krishakmart.onrender.com/api",
   withCredentials: true,
 });
